009e16df4e610123c3dd835716ae457fd399bb3f
ChengZi <chen.zhang@zilliz.com>
2023-12-11 14:53:32 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='evalparrot',
-      version='0.0.1',
+      version='0.0.2',
       description='parrot evaluation',
       url='https://github.com/zc277584121/evalparrot.git',
       author='zc277584121',
